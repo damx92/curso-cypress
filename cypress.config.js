ece01027b1862543770b0b31a26c794b2e54c341
damx92 <damiancouto1992@gmail.com>
2025-02-12 16:19:14 -0300

    config allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
+import { configureAllureAdapterPlugins } from '@mmisty/cypress-allure-adapter/plugins';
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
+      configureAllureAdapterPlugins(on, config);
       return config; 
     },
   },
