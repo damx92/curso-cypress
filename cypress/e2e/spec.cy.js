7048df6c75023eefc9348a13af959458f1738e3f
damx92 <damiancouto1992@gmail.com>
2025-03-25 12:51:05 -0300

    cart

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,3 @@
-import { HomeMethods } from "./pages/home/home.methods"
 import { LoginMethods } from "./pages/login/login.methods"
 
 describe('template spec', () => {
@@ -6,8 +5,9 @@ describe('template spec', () => {
     const usuario = 'shoppingcart'
     const contrasena = 'shop23'
     cy.visit('https://www.demoblaze.com/')
-   HomeMethods.clickOnProductLink('Iphone 6 32gb')
-   cy.wait(20000)
+   cy.wait(30000)
+   CartMethods.clickOnDeleteLink('Nokia lumia 1520')
+   cy.wait(30000)
   
   })
 })
